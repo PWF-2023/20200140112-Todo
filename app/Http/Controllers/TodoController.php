ec9c67a3781a63c0ee75ee3676a68d89e4780811
alfinks <maulanaafiansyah@gmail.com>
2023-05-18 20:11:36 +0700

    middleware, gate admin

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todo;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class TodoController extends Controller
+{
+    public function index()
+    {
+        $todos = Todo::where('user_id', auth()->user()->id)
+            ->orderBy('is_complete', 'asc')
+            ->orderBy('created_at', 'desc')
+            ->get();
+
+        // dd($todos);
+        $todosCompleted = Todo::where('user_id', auth()->user()->id)
+            ->where('is_complete', true)
+            ->count();
+        return view('todo.index', compact('todos', 'todosCompleted'));
+    }
+
+    public function create()
+    {
+        return view('todo.create');
+    }
+
+    public function edit(Todo $todo)
+    {
+        // Code lebih pendek dan tidak memerlukan else
+        if (auth()->user()->id == $todo->user_id) {
+            return view('todo.edit', compact('todo'));
+        }
+
+        // Bagian Else
+        return redirect()->route('todo.index')->with('danger', 'You are not authorized to edit this todo!');
+    }
+
+    public function update(Request $request, Todo $todo)
+    {
+        $request->validate([
+            'title' => 'required|max:255',
+        ]);
+        $todo->update([
+            'title' => ucfirst($request->title),
+        ]);
+        return redirect()->route('todo.index')->with('success', 'Todo updated successfully!');
+    }
+
+    public function complete(Todo $todo)
+    {
+        if (auth()->user()->id == $todo->user_id) {
+            $todo->update([
+                'is_complete' => true,
+            ]);
+            return redirect()->route('todo.index')->with('success', 'Todo completed successfully!');
+        }
+        return redirect()->route('todo.index')->with('danger', 'You are not authorized to complete this todo!');
+    }
+
+    public function uncomplete(Todo $todo)
+    {
+        if (auth()->user()->id == $todo->user_id) {
+            $todo->update([
+                'is_complete' => false,
+            ]);
+            return redirect()->route('todo.index')->with('success', 'Todo uncompleted successfully!');
+        }
+        return redirect()->route('todo.index')->with('danger', 'You are not authorized to uncomplete this todo!');
+    }
+
+    public function store(Request $request, Todo $todo)
+    {
+        $request->validate([
+            'title' => 'required|max:255',
+        ]);
+
+        // Practical
+        // $todo = new Todo;
+        // $todo->title = $request->title;
+        // $todo->user_id = auth()->user()->id;
+        // $todo->save();
+
+        // Query Builder way
+        // DB::table('todos)->insert([
+        // 'title' => $request->title,
+        // 'user_id' => auth()->user()->id,
+        // 'created_at' => now(),
+        // 'updated_at' => now(),
+        // ])
+
+        // Eloquent Way - Readable
+
+        $todo = Todo::create([
+            'title' => ucfirst($request->title),
+            'user_id' => auth()->user()->id,
+        ]);
+
+        return redirect()->route('todo.index')->with('success', 'Todo created successfully!');
+    }
+
+    public function destroy(Todo $todo)
+    {
+        if (auth()->user()->id == $todo->user_id) {
+            $todo->delete();
+            return redirect()->route('todo.index')->with('success', 'Todo deleted successfully!');
+        }
+        return redirect()->route('todo.index')->with('danger', 'You are not authorized to delete this todo!');
+    }
+
+    public function destroyCompleted()
+    {
+        $todoCompleted = Todo::where('user_id', auth()->user()->id)
+            ->where('is_complete', true)
+            ->get();
+        foreach ($todoCompleted as $todo) {
+            $todo->delete();
+        }
+        return redirect()->route('todo.index')->with('success', 'All completed todos deleted successfully!');
+    }
+}
---
+++ tugas7.md
@@ -0,0 +1,6 @@
+#Tugas 7
+
+## 1. Middleware & Gate Admin Not Active
+![Alt text](screenshoot/tugas7/ss.png)
+## 2. Middleware & Gate Admin Active
+![Alt text](screenshoot/tugas7/ss1.png)
